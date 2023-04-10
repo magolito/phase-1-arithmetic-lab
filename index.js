3a8655ed627c42722698c3cac9f9dcca2a45fe7e
Manuel Avila <magolitonft2311@gmail.com>
2023-04-10 16:42:37 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+
+const multiply = num1 * num2
+
+const random = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
+
+const num3= 20
+const num4= 16
+
+const mod = num3 % num4
+
+const max= Math.max(1,4,8,12,16,20)
+
+
